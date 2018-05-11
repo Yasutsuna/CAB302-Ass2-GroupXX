5f6a94cb716a757e2b95424b6a3fc0becd44f9f1
Yasutsuna <zhi.choon96@gmail.com>
2018-05-11 23:55:30 +1000

    Reverted Test

--- CAB302-Ass2/src/project/CSVReader.java
+++ CAB302-Ass2/src/project/CSVReader.java
@@ -23,3 +23,35 @@ public class CSVReader {
     } 
 
 }
+
+//CSVWriter (Not needed atm but reference)
+
+/*
+import java.io.BufferedWriter; 
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.io.FileWriter; 
+import java.io.IOException; 
+
+public class Test { 
+
+  public static void main(String[] args) { 
+    try { 
+      File csv = new File("F:/writers.csv"); // CSV folder
+
+      BufferedWriter bw = new BufferedWriter(new FileWriter(csv, true)); 
+     
+      bw.write("name" + "," + "1988" + "," + "1992"); 
+      bw.newLine(); 
+      bw.close(); 
+
+    } catch (FileNotFoundException e) { 
+      
+      e.printStackTrace(); 
+    } catch (IOException e) { 
+      
+      e.printStackTrace(); 
+    } 
+  }
+}
+*/
