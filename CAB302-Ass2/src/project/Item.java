840c61ca7acfa6e62bf37132b964108d1c9b1ba0
jeffrey0528 <jeffrey0528@users.noreply.github.com>
2018-05-22 13:14:48 +1000

    reading the csv file correction

--- CAB302-Ass2/src/project/Item.java
+++ CAB302-Ass2/src/project/Item.java
@@ -8,12 +8,22 @@ public class Item {
 	private int amount;
 	private int temp = 1; //Not all need this
 	
-	public Item(String name,double cost, double price, int reorder, int amount, int temp) {
+	public Item(String name,double cost, double price, int reorder, int amount) {
 		this.name = name;
 		this.cost = cost;
 		this.price = price;
 		this.reorder = reorder;
 		this.amount = amount;
+		
+	}
+	
+	public Item(String name2, double cost2, double price2, int reorder2, int amount2, int temp) {
+		// TODO Auto-generated constructor stub
+		this.name = name2;
+		this.cost = cost2;
+		this.price = price2;
+		this.reorder = reorder2;
+		this.amount = amount2;
 		this.temp = temp;
 	}
 	
